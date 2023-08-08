f8e61dc5d32b7f56923cebd597a70ccd629e2d45
Leonardo Fernández <leonardo99112821385@gmail.com>
2023-08-08 11:01:23 -0400

    JS almost ready

---
+++ src/script.js
@@ -0,0 +1,21 @@
+const connectButton = document.querySelector('.li-connect');
+const contactToggleDesk = document.querySelector('.contacts-desktop');
+const hambIcon = document.querySelector('.hamburger-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
+const contactsToggleMobile = document.querySelector('.contacts');
+const connectMobile = document.querySelector('.mobile-li');
+
+connectButton.addEventListener('click', contactDesk);
+hambIcon.addEventListener('click', menuToggle);
+connectMobile.addEventListener('click', connectButtonMobile);
+
+function contactDesk () {
+    contactToggleDesk.classList.toggle('inactive');
+}
+function menuToggle () {
+    mobileMenu.classList.toggle('inactive');
+}
+function connectButtonMobile () {
+    console.log('s');
+    contactsToggleMobile.classList.toggle('inactive');
+}
